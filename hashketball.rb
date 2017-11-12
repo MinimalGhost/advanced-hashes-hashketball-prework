7cc5a49769f184ceb58b57a574fa6922b7bcc3a3
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-12 20:18:52 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -110,10 +110,12 @@ def player_stats(player)
 end
 
 def big_shoe_rebounds
-    winner_chicken_dinner = ["name", 0, 0] 
+  winner_chicken_dinner = ["name", 0, 0] 
   game_hash.each_value do |team|
     team[:players].each do |name, player_data|
+      if player_data[:shoe] > winner_chicken_dinner[1]
 
+      end
     end
   end
 end
